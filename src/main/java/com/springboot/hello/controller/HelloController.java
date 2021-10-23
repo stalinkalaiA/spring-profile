eb3901cb8437dac2ff18f98bb7294f4a31a029f5
stalinkalaiA <53252357+stalinkalaiA@users.noreply.github.com>
2021-10-23 08:39:04 +0530

    Initial commit

---
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.springboot.hello.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+	
+	@Value("${spring.datasource.url}")
+	private String dbURL;
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		System.out.println("DB URL :: " + dbURL);
+		
+		return dbURL;
+	}
+
+}
